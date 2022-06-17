bb1ccbec86f60e62052baa41639514331fc38171
ytubeupX <86553360+ytubeupX@users.noreply.github.com>
2022-06-18 00:31:26 +0600

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -60,7 +60,7 @@ except:
 
 PORT = environ.get('PORT')
 Popen([f"gunicorn web.wserver:app --bind 0.0.0.0:{PORT}"], shell=True)
-srun(["last-api", "-d", "--profile=."])
+srun(["qbittorrent-nox", "-d", "--profile=."])
 if not ospath.exists('.netrc'):
     srun(["touch", ".netrc"])
 srun(["cp", ".netrc", "/root/.netrc"])
@@ -71,7 +71,7 @@ if TORRENT_TIMEOUT is not None:
         a.write(f"bt-stop-timeout={TORRENT_TIMEOUT}\n")
 with open("a2c.conf", "a+") as a:
     a.write(f"bt-tracker=[{trackers}]")
-srun(["extra-api", "--conf-path=/usr/src/app/a2c.conf"])
+srun(["aria2c", "--conf-path=/usr/src/app/a2c.conf"])
 alive = Popen(["python3", "alive.py"])
 sleep(0.5)
 
